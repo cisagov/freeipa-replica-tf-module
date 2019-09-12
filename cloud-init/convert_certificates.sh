484d5832f340a85efe57b4271e6f6b4f7107ffd4
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-09-12 13:12:27 -0400

    Include entire certificate chain when constructing PKCS#12 certificate
    FreeIPA insists on having the ENTIRE certificate chain be present.

--- cloud-init/convert_certificates.sh
+++ cloud-init/convert_certificates.sh
@@ -7,6 +7,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# FreeIPA insists that the ENTIRE certificate chain be present, all
+# the way back to the self-signed root cert.  Let's Encypt doesn't
+# include all that, so we need to grab the missing pieces.
+#
+# See https://letsencrypt.org/certificates/ for more details.
+curl -o /etc/ipa/isrgrootx1.pem \
+     https://letsencrypt.org/certs/isrgrootx1.pem.txt
+curl -o /etc/ipa/trustid-x3-root.pem \
+     https://letsencrypt.org/certs/trustid-x3-root.pem.txt
+
 # Convert the PEMs to PKCS#12 format
 #
 # The "${cert_pw}" item below that looks like a shell variable but is
@@ -18,4 +28,6 @@ openssl pkcs12 -export \
         -out /etc/ipa/cert.p12 \
         -in /etc/ipa/fullchain.pem \
         -inkey /etc/ipa/privkey.pem \
-        -passout pass:"${cert_pw}"
+        -passout pass:"${cert_pw}" \
+        -certfile /etc/ipa/isrgrootx1.pem \
+        -certfile /etc/ipa/trustid-x3-root.pem
